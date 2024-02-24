cf5ed76a7f4c9f4c6b2975324d0db96949700a6c
Victor Chevillotte <46164574+victor-chevillotte@users.noreply.github.com>
2024-02-24 22:42:45 +0100

    Ajouter une fonction de fermeture pour arrêter la boucle d'événements

--- new/main.py
+++ new/main.py
@@ -3,6 +3,11 @@ import aiotkinter
 from Frames.login_form import init_login_frame
 from ttkthemes import ThemedTk
 
+
+async def close_program(loop):
+    loop.stop()
+
+
 if __name__ == "__main__":
     asyncio.set_event_loop_policy(aiotkinter.TkinterEventLoopPolicy())
     loop = asyncio.get_event_loop()
@@ -10,6 +15,19 @@ if __name__ == "__main__":
     root_window = ThemedTk(theme="arc")
     root_window.geometry("1200x800")
     root_window.title("Trackloisirs")
+
+    # Initialisation de la fenêtre de connexion
     init_login_frame(root_window)
 
-    loop.run_forever()
+    # Définir une fonction de fermeture qui arrête la boucle d'événements
+    def on_close():
+        asyncio.create_task(close_program(loop))
+        root_window.destroy()
+
+    # Lier l'événement de fermeture de la fenêtre à la fonction on_close
+    root_window.protocol("WM_DELETE_WINDOW", on_close)
+
+    try:
+        loop.run_forever()
+    finally:
+        loop.close()
