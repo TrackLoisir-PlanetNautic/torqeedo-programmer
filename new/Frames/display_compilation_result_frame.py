4986ed61ac37012b431b6ece124e0f7460e61a27
Victor Chevillotte <victor.chevillotte@gmail.com>
2024-03-10 22:13:52 +0100

    Ajouter l'étiquette de résultat de compilation

--- new/Frames/display_compilation_result_frame.py
+++ new/Frames/display_compilation_result_frame.py
@@ -11,6 +11,9 @@ def render_display_compilation_result_frame(
     show_error_label = Label(right_column_frame, text="Error: ")
     show_error_label.pack(padx=10, pady=10)
 
+    compilation_result_label = Label(right_column_frame, text="")
+    compilation_result_label.pack(padx=10, pady=10)
+
     def restart_esp_and_get_infos_status():
         if torqeedo_programmer.selected_controller is None:
             show_error_label.config(text="Error: No controller selected")
@@ -19,12 +22,18 @@ def render_display_compilation_result_frame(
                 text="Error: Please connect to the controller"
             )
         elif (
-            torqeedo_programmer.selected_controller.compilation_result
-            is None
+            torqeedo_programmer.selected_controller.compilation_result is None
         ):
             show_error_label.config(
                 text="Error: No compilation result, restart esp"
             )
+        else:
+            show_error_label.config(text="")
+            compilation_result: CompilationResult = (
+                torqeedo_programmer.selected_controller.compilation_result
+            )
+            for line in compilation_result.lines:
+                compilation_result_label.config(text=line)
 
         right_column_frame.after(
             100, restart_esp_and_get_infos_status
