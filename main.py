58580db0319d184b58fa770ba5c51121bcda97e2
Victor CHEVILLOTTE <victor.chevillotte.ext@veolia.com>
2024-05-21 12:10:10 +0200

    chore: Update main.py to integrate Tkinter's main loop with asyncio

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-import aiotkinter
 from Frames.login_form import init_login_frame
 from ttkthemes import ThemedTk
 
@@ -8,13 +7,14 @@ async def close_program(loop):
     loop.stop()
 
 
-if __name__ == "__main__":
-    asyncio.set_event_loop_policy(aiotkinter.TkinterEventLoopPolicy())
+async def tkinter_mainloop(root_window):
+    while True:
+        root_window.update()
+        await asyncio.sleep(0.01)
 
-    # Create a new event loop and set it as the current event loop
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
 
+async def main():
+    # Initialize the Tkinter root window
     root_window = ThemedTk(theme="arc")
     root_window.geometry("1200x800")
     root_window.title("Trackloisirs")
@@ -23,14 +23,26 @@ if __name__ == "__main__":
     init_login_frame(root_window)
 
     # Define a function to stop the event loop and close the window
-    def on_close():
-        asyncio.create_task(close_program(loop))
+    async def on_close():
+        await close_program(loop)
         root_window.destroy()
 
     # Bind the window close event to the on_close function
-    root_window.protocol("WM_DELETE_WINDOW", on_close)
+    root_window.protocol(
+        "WM_DELETE_WINDOW", lambda: asyncio.create_task(on_close())
+    )
 
+    # Integrate Tkinter's main loop with asyncio
+    await tkinter_mainloop(root_window)
+
+
+if __name__ == "__main__":
+    # Create a new event loop and set it as the current event loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+
+    # Run the main coroutine in the event loop
     try:
-        loop.run_forever()
+        loop.run_until_complete(main())
     finally:
         loop.close()
